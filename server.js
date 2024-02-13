a912401fff485e3430d3e9733b2fb4f8bb9af956
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2024-02-13 15:09:26 +0530

    Add server.js with /ping route

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/ping', (req, res) => {
+  res.json({ message: 'pong' });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 server running on PORT: ${port}`);
+});
+
+module.exports = app;
