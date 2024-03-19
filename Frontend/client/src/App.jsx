faf8c274ea05e4668c2b4bb9b18c453a66333faa
ABHIKALVIUM <144119494+ABHIKALVIUM@users.noreply.github.com>
2024-03-19 16:43:57 +0530

    Added created_by functionality

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -9,29 +9,63 @@ import RegisterForm from '../Component/Register';
 import LoginForm from '../Component/Login';
 
 function App() {
-
+  const [mainData, setMainData] = useState(0);
   const [data, setData] = useState(0);
   const [form,setForm]=useState(true);
   const [flag,setFlag]=useState(true);
   const [login,setLogin]=useState(false);
+  let createdBy=[];
 
+  useEffect(()=>{
+    fetch(`${api_url}/GET`)
+     .then(res=> res.json())
+     .then(res=>{
+      setData(res);
+      setMainData(res);
+      
+      res.map((ele)=>{
+        let flag=true;
+        if(createdBy.length==0){
+          createdBy.push(ele.created_by)
+        }
+        else{
+          for(let i=0;i<createdBy.length;i++){
+            // console.log(ele.created_by==createdBy[i],createdBy)
+            if(ele.created_by==createdBy[i]){
+              flag=false;
+              break;
+            }
+          }
+          if(flag){
+            createdBy.push(ele.created_by)
+          }
+        }
+      });
+      console.log(createdBy)
+     })
+     .catch(err=>{
+      console.log(err);
+     })
+  },[]);
 
   useEffect(()=>{
      fetch(`${api_url}/GET`)
      .then(res=> res.json())
      .then(res=>{
-      console.log(res)
-      setData(res)
+      setData(res);
+      setMainData(res)
      })
      .catch(err=>{
       console.log(err);
      })
-  },[flag])
+  },[flag]);
+
+  
 
   return(
     <>
-      <Routes>
-        <Route path="/" element={<Home data={data} setFlag={setFlag} flag={flag} login={login} setLogin={setLogin}/>}></Route>
+      <Routes>  
+        <Route path="/" element={<Home data={data} setData={setData} mainData={mainData} createdBy={createdBy} setFlag={setFlag} flag={flag} login={login} setLogin={setLogin}/>}></Route>
         <Route path="/post_data" element={<Form/>}></Route>
         <Route path="/update_data/:id" element={<UpdateForm />}></Route>
         <Route path="/register" element={<RegisterForm />}></Route>
